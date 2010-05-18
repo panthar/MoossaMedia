1ff69853237ed53c879b186fa9a7312c8e77f4ac
Robert H <panthar@gmail.com>
2010-05-18 16:42:23 -0500

    Added initial media

---
+++ main.lua
@@ -0,0 +1,6 @@
+local smed = LibStub("LibSharedMedia-3.0")
+smed:Register("statusbar", "Flat Smooth", "Interface\\AddOns\\MoossaMedia\\bar\\flatsmooth.tga")
+smed:Register("statusbar", "Curved Bar", "Interface\\AddOns\\MoossaMedia\\bar\\curvedbar.tga")
+smed:Register("statusbar", "Steel", "Interface\\AddOns\\MoossaMedia\\bar\\Steel.tga")
+smed:Register("font", "Franklin Gothic Medium", "Interface\\AddOns\\MoossaMedia\\font\\font1.ttf")
+smed:Register("border", "Square Outline", "Interface\\AddOns\\MoossaMedia\\border\\squareline.tga")
