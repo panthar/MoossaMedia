583d85605eefda9ead48d0d11bc54bf66386fbf1
Robert H <panthar@gmail.com>
2010-05-18 16:50:20 -0500

    Added nice corner border and cleaned up .lua loader

--- main.lua
+++ main.lua
@@ -1,6 +1,13 @@
 local smed = LibStub("LibSharedMedia-3.0")
+
+-- Bars
 smed:Register("statusbar", "Flat Smooth", "Interface\\AddOns\\MoossaMedia\\bar\\flatsmooth.tga")
 smed:Register("statusbar", "Curved Bar", "Interface\\AddOns\\MoossaMedia\\bar\\curvedbar.tga")
 smed:Register("statusbar", "Steel", "Interface\\AddOns\\MoossaMedia\\bar\\Steel.tga")
+
+-- Fonts
 smed:Register("font", "Franklin Gothic Medium", "Interface\\AddOns\\MoossaMedia\\font\\font1.ttf")
+
+-- Borders
 smed:Register("border", "Square Outline", "Interface\\AddOns\\MoossaMedia\\border\\squareline.tga")
+smed:Register("border", "Corners 1pt", "Interface\\AddOns\\MoossaMedia\\border\\corners1pt.tga")
